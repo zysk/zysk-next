b0d5dc6cb7d5233cf427e4ffe3eb69726aea4a36
Varun <varun.umesh@inboundsys.com>
2022-02-11 15:38:44 +0530

    Worked on contact form functionality & other UI Changes

---
+++ pages/_document.js
@@ -0,0 +1,58 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getServerSideProps(ctx) {
+    const initialProps = await Document.getServerSideProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const name = 'Gojek';
+    return (
+      <Html lang="en">
+        <Head>
+       
+          {/* Enable GTM only for production environment */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-66233587-1`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-66233587-1');
+         `,
+            }}
+          />
+        </Head>
+        <body>
+
+          {/* End Google Tag Manager (noscript) */}
+          <Main />
+          <NextScript />
+          {process.env.env == 'PROD' && (
+            <>
+              <script
+                src="https://t.ghostboard.io/min.js"
+                async
+                defer
+                data-gbid="609a7c953a29de7474d49404"
+              ></script>
+              <noscript>
+                <img
+                  src="https://ghostboard.io/api/noscript/609a7c953a29de7474d49404/pixel.gif"
+                  alt="Ghostboard pixel"
+                />
+              </noscript>
+            </>
+          )}
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/api/mail.js
@@ -0,0 +1,32 @@
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default async (req, res) => {
+  const body = JSON.parse(req.body);
+
+  const message = `
+   <div>
+   <h3 style="font-size:17px;font-weight:600">Hey there!<h3>
+   <p class="font-size:15px;line-height:24px">You just received a new submission to your <span style="font-size:16px;font-weight:600">Zysk Contact form form.</span> <br/> Here is the data that was collected...
+   </p>
+   </div>
+    Name : ${body.name}\r\n
+    Email : ${body.email}\r\n
+    Phone : ${body.phone}\r\n
+    Message : ${body.message}
+  `;
+
+  const data = {
+    to: 'contact@zysk.in',
+    from: 'contact@zysk.in',
+    subject: `Alert..! Contact form submission`,
+    text: message,
+    html: message.replace(/\r\n/g, '<br />'),
+  };
+
+  await mail.send(data);
+
+  res.status(200).json({ status: 'OK' });
+};
+
