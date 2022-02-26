76ac1e220efd4edc06de4620f24d882c431ced3f
Shilpa Gowda <shilpavp229@gmail.com>
2022-02-27 00:33:02 +0530

    refactor: header code optimization
    Create separate component for lnk and optimized code
    Used Link tag instead of a tag

--- components/common/header.js
+++ components/common/header.js
@@ -1,117 +1,51 @@
-import React, { Fragment, useState } from "react";
-import { Popover, Transition } from "@headlessui/react";
+import React, { Fragment, useEffect, useState } from "react";
+import { Popover, Transition, Dialog } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
 import { links } from "./data";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+import HeaderLink from "./header-link";
 
 const Header = () => {
   const router = useRouter();
-  const [show, setShow] = useState("false");
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const { pathname } = router;
 
-  const handleToggle = () => {
-    setShow(!show);
-  };
+  useEffect(() => {
+    setSidebarOpen(false);
+  }, [pathname]);
 
   return (
     <Popover className="relative bg-white" id="navbarNav">
       <div className="mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center py-2 lg:justify-start">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="/">
-              <img className="h-14 w-auto sm:h-16" src="/img/logo.png" alt="" />
-            </a>
+            <Link href="/">
+              <a>
+                <img
+                  className="h-14 w-auto sm:h-16"
+                  src="/img/logo.png"
+                  alt="Zysk Technologies"
+                />
+              </a>
+            </Link>
           </div>
-          <div className="-mr-2 -my-2 lg:hidden">
-            <Popover.Button
-              className="bg-white rounded-md inline-flex items-center justify-center 
-            text-gray-400 hover:text-gray-500 hover:bg-gray-100"
+          <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 ">
+            <button
+              type="button"
+              className="px-4 text-gray-500 md:hidden"
+              onClick={() => setSidebarOpen(true)}
             >
-              <span className="sr-only">Open menu</span>
-              <MenuIcon className="h-6 w-6" aria-hidden="true" />
-            </Popover.Button>
+              <span className="sr-only">Open sidebar</span>
+              <MenuIcon className="h-6 w-6 active" aria-hidden="true" />
+            </button>
           </div>
-          <Popover.Group as="nav" className="hidden lg:flex space-x-16">
+          <Popover.Group as="nav" className="hidden lg:flex space-x-14">
+            {/* Loop through menu links and list */}
             {links.map((link) => (
               <React.Fragment key={link.id}>
-                {/* If a nav link is not having children */}
-                {!link.children && (
-                  <Link href={link.url}>
-                    <a
-                      className={
-                        pathname === link.url ? "active nav-item" : "nav-item"
-                      }
-                      rel={link.type === "external" ? "noreferrer" : ""}
-                      target={link.type === "external" ? "_blank" : ""}
-                    >
-                      {link.name}
-                    </a>
-                  </Link>
-                )}
-
-                {/* If a nav link has children */}
-
-                {link.children && (
-                  <Popover className="relative">
-                    {({ open }) => (
-                      <React.Fragment>
-                        <Popover.Button
-                          className={classNames(
-                            open ? "text-gray-900" : "text-gray-500",
-                            "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900"
-                          )}
-                        >
-                          <Link
-                            href={link.url}
-                            className={
-                              pathname.startsWith(link.url)
-                                ? "active nav-item dropdown-toggle"
-                                : "nav-item dropdown-toggle"
-                            }
-                          >
-                            <a>{link.name}</a>
-                          </Link>
-                        </Popover.Button>
-
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-200"
-                          enterFrom="opacity-0 translate-y-1"
-                          enterTo="opacity-100 translate-y-0"
-                          leave="transition ease-in duration-150"
-                          leaveFrom="opacity-100 translate-y-0"
-                          leaveTo="opacity-0 translate-y-1"
-                        >
-                          <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-56 sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                            <div className="ring-1 rounded-sm ring-black ring-opacity-5 overflow-hidden">
-                              <div className="relative grid gap-3 bg-white px-4 py-3">
-                                {link.children.map((child) => (
-                                  <Link key={child.id} href={child.url}>
-                                    <a
-                                      className={
-                                        pathname === child.url
-                                          ? "active text-base text-gray-600"
-                                          : "text-base text-gray-600"
-                                      }
-                                    >
-                                      {child.name}
-                                    </a>
-                                  </Link>
-                                ))}
-                              </div>
-                            </div>
-                          </Popover.Panel>
-                        </Transition>
-                      </React.Fragment>
-                    )}
-                  </Popover>
-                )}
+                <HeaderLink data={link} pathname={pathname} />
               </React.Fragment>
             ))}
           </Popover.Group>
@@ -120,92 +54,81 @@ const Header = () => {
 
       {/* Mobile header */}
 
-      <Transition
-        as={Fragment}
-        enter="duration-200 ease-out"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100"
-        leave="duration-100 ease-in"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
-      >
-        <Popover.Panel
-          focus
-          className="absolute top-0 inset-x-0 transition transform origin-top-right lg:hidden"
+      <Transition.Root show={sidebarOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 flex z-40 md:hidden"
+          onClose={setSidebarOpen}
         >
-          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-            <div className="pt-5 pb-6 px-5">
-              <div className="flex items-center justify-between">
-                <div>
-                  <img
-                    className="h-14 w-auto"
-                    src="/img/logo.png"
-                    alt="Zysk Technologies"
-                  />
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <div className="relative flex-1 flex flex-col max-w-sm w-full pt-5 pb-4 bg-white">
+              {/* Open sidebar when clicked on Menu icon */}
+              <Transition.Child
+                as={Fragment}
+                enter="ease-in-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="absolute top-0 right-0 -mr-12 pt-2">
+                  <button
+                    type="button"
+                    className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    onClick={() => setSidebarOpen(false)}
+                  >
+                    <span className="sr-only">Close sidebar</span>
+                    <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                  </button>
                 </div>
-                <div className="-mr-2">
-                  <Popover.Button className="bg-white rounded-md  inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
-                    <span className="sr-only">Close menu</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
+              </Transition.Child>
+
+              {/* Logo */}
+              <div className="flex-shrink-0 flex items-center px-4">
+                <img
+                  className="h-16 w-auto"
+                  src="/img/logo.png"
+                  alt="Zysk Technologies"
+                />
               </div>
-              <div className="mt-6">
-                <nav className="grid gap-y-5">
+
+              <div className="mt-5 flex-1 h-0 overflow-y-auto">
+                <nav className="px-2 space-y-1">
+                  {/* loop through menu links and list */}
                   {links.map((link) => (
                     <React.Fragment key={link.id}>
-                      {!link.children && (
-                        <Link key={link.id} href={link.url}>
-                          <a
-                            className={
-                              pathname === link.url
-                                ? "active nav-item"
-                                : "nav-item"
-                            }
-                            rel={link.type === "external" ? "noreferrer" : ""}
-                            target={link.type === "external" ? "_blank" : ""}
-                          >
-                            {link.name}
-                          </a>
-                        </Link>
-                      )}
-
-                      {link.children && (
-                        <div>
-                          <a
-                            href="#"
-                            onClick={handleToggle}
-                            className={
-                              pathname.startsWith(link.url)
-                                ? "active nav-item dropdown-toggle"
-                                : "nav-item dropdown-toggle"
-                            }
-                          >
-                            {link.name}
-                          </a>
-                          <div className={show ? "hidden" : "pt-2"}>
-                            {link.children.map((child) => (
-                              <Link key={child.id} href={child.url}>
-                                <a
-                                  className={`pl-3 pt-1 block text-sm font-semibold ${
-                                    pathname === child.url ? "active" : ""
-                                  }`}
-                                >
-                                  {child.name}
-                                </a>
-                              </Link>
-                            ))}
-                          </div>
-                        </div>
-                      )}
+                      <HeaderLink data={link} pathname={pathname} />
                     </React.Fragment>
                   ))}
                 </nav>
               </div>
             </div>
-          </div>
-        </Popover.Panel>
-      </Transition>
+          </Transition.Child>
+        </Dialog>
+      </Transition.Root>
+
+      {/* Static sidebar for desktop */}
+      <div className="md:pl-64 flex flex-col flex-1"></div>
     </Popover>
   );
 };
